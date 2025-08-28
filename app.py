3bee855bc0573f90068a628163b456ad6c10b8a7
Brand0nS <brandonscottwork12@gmail.com>
2025-08-28 15:22:33 -0400

    Added app.py and stack.py (A stack implementation test)

---
+++ app.py
@@ -0,0 +1,11 @@
+from stack import Stack
+
+if __name__ == '__main__':
+    print("Hello there!")
+    my_stack = Stack()
+    my_stack.push(1)
+    my_stack.push(2)
+    x = my_stack.pop()
+    print(x)
+    x = my_stack.pop()
+    print(x)
---
+++ stack.py
@@ -0,0 +1,12 @@
+class Stack:
+   def __init__(self):
+      self.content = []
+    
+   def push(self, x: int) -> None:
+      self.content.append(x)
+      
+   def pop(self) -> int:
+      answer = self.content[-1]
+      self.content = self.content[:-1:]
+      return answer
+   
