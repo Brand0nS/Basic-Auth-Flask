ef97791ff32f9cec5666b5333f70fd7ff9dae0f2
brand0ns <brandonscottwork12@gmail.com>
2025-09-01 10:28:21 -0400

    Database works! Added testing statement, and obscured password on html page via the password input type. (still in plaintext in the background). Now, all that's left to do is to hash and salt the password.

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.method == 'GET':
+    if request.method == 'GET':  # When a GET request is sent, Flask renders the index.html file in templates
         return render_template('index.html') #if it is a get method, startup the form
     elif request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         my_database.add_user(username, password)
+        return f"User {username} added successfully! Current database: {my_database.data}"
 
 if __name__ == '__main__':
     my_database = Database()
     app.run(host='0.0.0.0',debug=True)    #turn to false once you actually make the application (0.0.0.0 allows you to do both local and global through the network)
-    my_database.add_user('Brandon', 'password123')
     print(my_database.data)
     print(my_database.get_password())
     print(my_database.get_username())
