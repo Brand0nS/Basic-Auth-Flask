0ceeea13c3917b4b49ac2d7a39e3a71c89ae3ebd
brand0ns <brandonscottwork12@gmail.com>
2025-09-03 15:44:14 -0400

    Cleaned up code from prior additions. Added Argon2 for password hashing. Changed the verify user method to be a double check: (check user first and then the password hash, instead of checking both at once)

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
+from argon2 import PasswordHasher
+
 class Database:
-    username = ''
-    password = ''
     def __init__(self):
         self.data = {}
+        self.ph = PasswordHasher()
 
     def add_user(self, username, password) -> None:
-        self.data[username] = password
-        self.username = username
-        self.password = password
+        hash = self.ph.hash(password)
+        self.data[username] = hash
+       
 
     def get_username(self) -> str: 
         return self.username
@@ -15,8 +16,12 @@ class Database:
     def get_password(self) -> str: 
         return self.password
     
-    def verify_user(self, username, password) -> bool: # This will be replaced with hashing later on.
-        if(username in self.data and self.data[username] == password):
-            return True
-        
+    def verify_user(self, username, password) -> bool: 
+        if username not in self.data: # if the passed in username isn't in the database, return false
+            return False
+        try:
+            return self.ph.verify(self.data[username], password) #check the password against the database (try catch)
+        except Exception: # have to use the verify function, since hashes and salts are different each time. 
+            return False
+
     
